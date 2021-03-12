3c49768c41fc58b0ee8ae76f853e9f00d8e1f379
McBean29 <bfreeman0@gmail.com>
2021-03-12 23:33:35 +0000

    Checks if today has already been run, if it has it will return with previous details. If it wasn't run previously it will provide an updated list and adds it to the csv.

--- main.py
+++ main.py
@@ -1,42 +1,65 @@
-import datetime
-from datetime import datetime
+import csv
+import os
 import random
+import sys
 from _csv import writer
-from os import path
-import csv
-
-if not path.exists("file.csv"):
-    with open('file.csv', 'wb') as csvfile:
-        filewriter = csv.writer(csvfile, delimiter=',',
-                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        filewriter.writerow(['After Work', 'Before Bed', 'Date'])
-
-current_time = datetime.now().date()
-current_time = str(current_time)
-
-import csv
 from datetime import datetime
 
+global after_work
+global before_bed
+global csv_new_row
+
+
+# Check if CSV files exist
+def check_csv():
+    if not os.path.exists('log.csv'):
+        with open('log.csv', 'wb') as csv_file:
+            file_writer = csv.writer(csv_file, delimiter=',',
+                                     quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            file_writer.writerow(['Afternoon', 'Evening', 'Date'])
+    if not os.path.exists('activities.csv'):
+        with open('activities.csv', 'wb') as csv_file:
+            file_writer = csv.writer(csv_file, delimiter=',',
+                                     quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            file_writer.writerow(['Afternoon', 'Evening'])
+
+
 current_time = datetime.now().date()
 current_time = str(current_time)
 
-with open('file.csv', 'rt') as f:
-    reader = csv.reader(f, delimiter=',')
-    for row in reader:
-        if current_time == row[2]:  # if the username shall be on column 3 (-> index 2)
-            print(row)
 
-y = ["Python", "YouTube", "Read"]
-x = ["Assetto Corsa", "Kingdom Deliverance", "CitiXXl"] + y
+def already_run():
+    with open('log.csv', 'rt') as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+            if row[1] == current_time:
+                final_line = f.readlines()[-1]
+                sys.exit(final_line)
+            else:
+                break
 
-after_work = random.choice(x)
-before_bed = random.choice(y)
 
-while after_work == before_bed:
-    after_work = random.choice(x)
+def random_select():
+    # y = ["Python", "YouTube", "Read"]
+    # x = ["Assetto Corsa", "Kingdom Deliverance", "CitiXXl"] + y
+    import pandas
 
-csv_new_row = [after_work, before_bed, current_time]
+    import pandas as pd
 
+    dict_from_csv = pd.read_csv('activities.csv', header=0, squeeze=True, index=False).to_dict()
+    print(dict_from_csv)
+
+    global after_work
+    global before_bed
+    after_work = random.choice(list(dict_from_csv.items()))
+    before_bed = random.choice(list(dict_from_csv.values()))
+
+    while after_work == before_bed:
+        after_work = random.choice(list(dict_from_csv.values()))
+    global csv_new_row
+    csv_new_row = [after_work, before_bed, current_time]
+    after_work = str(after_work)
+    before_bed = str(before_bed)
 
 def append_list_as_row(file_name, list_of_elem):
     # Open file in append mode
@@ -47,6 +70,10 @@ def append_list_as_row(file_name, list_of_elem):
         csv_writer.writerow(list_of_elem)
 
 
-print(
-    "Current Time = " + current_time + " " + "\nAfter Work Activity = " + after_work + "\nBefore Bed Activity = " + before_bed)
-append_list_as_row('file.csv', csv_new_row)
+check_csv()
+already_run()
+random_select()
+print("Current Time = " + current_time + " " + "\nAfter Work Activity = " + after_work)
+print("Before Bed Activity = " + before_bed)
+append_list_as_row('log.csv', csv_new_row)
+sys.exit()
